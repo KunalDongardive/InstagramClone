99ffb87a23f69767a8ebd8f16e3c84c0c25383fe
Kunal Dongardive <kunaldongardive358@gmail.com>
2024-06-08 01:53:50 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import "./App.css";
-// import StoriesPosts from "./components/stories/StoriesPosts";
-// import MenuBar from "./components/menuBar/MenuBar";
-import HomePage from "./pages/homePage/HomePage";
+import Search from "./components/search/Search";
+
+// import HomePage from "./pages/homePage/HomePage";
 // import SignIn from "./pages/signIn/SignIn";
 // import SignUp from "./pages/signUp/SignUp";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      {/* Hello World <FontAwesomeIcon icon={faInstagram} /> */}
       {/* <SignUp /> */}
       {/* <SignIn /> */}
-      <HomePage />
-      {/* <MenuBar /> */}
-      {/* <StoriesPosts /> */}
+      {/* <HomePage /> */}
+      <Search />
     </div>
   );
 }

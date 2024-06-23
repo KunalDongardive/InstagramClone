f9c39614c18dfe15fbf8965ca9341defe785b3b8
Kunal Dongardive <kunaldongardive358@gmail.com>
2024-06-23 23:01:34 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import "./App.css";
 // import Reels from "./components/reels/Reels";
 // import Feed from "./components/feed/Feed";
 // import Search from "./components/search/Search";
-import HomePage from "./pages/homePage/HomePage";
-// import SignIn from "./pages/signIn/SignIn";
+// import HomePage from "./pages/homePage/HomePage";
+import SignIn from "./pages/signIn/SignIn";
 // import SignUp from "./pages/signUp/SignUp";
 
 function App() {
   return (
     <div className="App">
       {/* <SignUp /> */}
-      {/* <SignIn /> */}
-      <HomePage />
+      <SignIn />
+      {/* <HomePage /> */}
       {/* <Search /> */}
       {/* <Feed /> */}
       {/* <Reels /> */}
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -9,12 +9,9 @@ import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faSquarePlus } from "@fortawesome/free-regular-svg-icons";
 import { faThreads } from "@fortawesome/free-brands-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
-import { faComment } from "@fortawesome/free-regular-svg-icons";
-import { faBookmark } from "@fortawesome/free-regular-svg-icons";
-import { faShare } from "@fortawesome/free-solid-svg-icons";
-import { faCircle } from "@fortawesome/free-solid-svg-icons";
+
 import "./homePage.css";
+import Post from "../../components/post/Post";
 
 const HomePage = () => {
   return (
@@ -58,121 +55,7 @@ const HomePage = () => {
           </div>
         </div>
         <div className="container">
-          <div className="stories-Posts">
-            <div className="stories">
-              <div className="profile">
-                <img
-                  src="./images/own.jpg"
-                  alt=""
-                  height={"50px"}
-                  width={"50px"}
-                  style={{ objectFit: "cover", borderRadius: "50%" }}
-                />
-                <div className="userName">kunal_d_3</div>
-              </div>
-            </div>
-            <div className="posts">
-              <div className="top">
-                <div className="topLeft">
-                  <div className="profile">
-                    <img
-                      src="./images/own.jpg"
-                      alt=""
-                      height={"50px"}
-                      width={"50px"}
-                      style={{ objectFit: "cover", borderRadius: "50%" }}
-                    />
-                  </div>
-                  <div className="userName">
-                    <div className="origin">
-                      <div className="name">kunal_d_3</div>
-                      <div className="origin">India</div>
-                    </div>
-                    <div className="dot">
-                      <FontAwesomeIcon
-                        icon={faCircle}
-                        style={{ fontSize: "0.2rem" }}
-                      />
-                    </div>
-                    <div className="time">5h</div>
-                  </div>
-                </div>
-                <div className="right">
-                  <FontAwesomeIcon icon={faEllipsis} />
-                </div>
-              </div>
-              <div className="center">
-                <img
-                  src="./images/4756.jpg"
-                  alt=""
-                  height={"450px"}
-                  width={"585px"}
-                  style={{ objectFit: "cover" }}
-                />
-              </div>
-              <div className="bottom">
-                <div className="options">
-                  <div className="optionsLeft">
-                    <div className="like">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </div>
-                    <div className="comments">
-                      <FontAwesomeIcon icon={faComment} />
-                    </div>
-                    <div className="share">
-                      <FontAwesomeIcon icon={faShare} />
-                    </div>
-                  </div>
-                  <div className="right">
-                    <FontAwesomeIcon icon={faBookmark} />
-                  </div>
-                </div>
-                <div className="likes" style={{ padding: "0.2rem 0rem" }}>
-                  358 likes
-                </div>
-                <div className="caption">
-                  <div className="summary">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Deserunt, commodi?Lorem ipsum dolor sit amet consectetur,
-                    adipisicing elit. Expedita inventore quaerat magnam
-                    accusamus quasi adipisci voluptates amet quisquam nulla sed!
-                  </div>
-                  <div className="viewComment">View all comments</div>
-                  <div className="addComment">
-                    <textarea
-                      name=""
-                      id=""
-                      cols="20"
-                      rows="1"
-                      placeholder="Add a comment"
-                    ></textarea>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="suggestions">
-            <div className="suggLeft">
-              <div className="profile">
-                <img
-                  src="./images/own.jpg"
-                  alt=""
-                  height={"50px"}
-                  width={"50px"}
-                  style={{ objectFit: "cover", borderRadius: "50%" }}
-                />
-              </div>
-              <div className="userName">
-                <div className="origin">
-                  <div className="ame">kunal_d_3</div>
-                  <div className="origin">Kunal Dongardive</div>
-                </div>
-              </div>
-            </div>
-            <div className="right">
-              <div className="sBtn">Switch</div>
-            </div>
-          </div>
+          <Post />
         </div>
       </div>
       <div className="footer"></div>

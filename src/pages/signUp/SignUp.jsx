102f66c31565505e56904e2229eb4b13cfeb3ded
Kunal Dongardive <kunaldongardive358@gmail.com>
2024-07-13 23:34:07 +0530

    commit

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -82,82 +82,89 @@ const SignUp = () => {
 
   return (
     <div className="signUp">
-      <div className="page">
-        <div className="header">
-          <h1 className="logo">𝓘𝓷𝓼𝓽𝓪𝓰𝓻𝓪𝓶</h1>
-          <p style={{ fontWeight: "bold", color: "#8e8e8e" }}>
-            Sign up to see photos and videos <br /> from your friends.
-          </p>
-          <button>
-            <FontAwesomeIcon
-              icon={faSquareFacebook}
-              style={{ fontSize: "20px" }}
-            />
-            &nbsp; Log in with Facebook
-          </button>
-          <div className="hline">
-            <div className="line"></div> OR <div className="line"></div>
+      <div className="container">
+        <div className="page">
+          <div className="header">
+            <h1 className="logo">𝓘𝓷𝓼𝓽𝓪𝓰𝓻𝓪𝓶</h1>
+            <p style={{ fontWeight: "bold", color: "#8e8e8e" }}>
+              Sign up to see photos and videos <br /> from your friends.
+            </p>
+            <button>
+              <FontAwesomeIcon
+                icon={faSquareFacebook}
+                style={{ fontSize: "20px" }}
+              />
+              &nbsp; Log in with Facebook
+            </button>
+            <div className="hline">
+              <div className="line"></div> OR <div className="line"></div>
+            </div>
+          </div>
+          <div className="container">
+            <form>
+              <input
+                type="text"
+                name="email"
+                placeholder="Email"
+                ref={emailRef}
+              />
+              <input
+                type="text"
+                name="name"
+                placeholder="Full Name"
+                ref={nameRef}
+              />
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                ref={userNameRef}
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                ref={passwordRef}
+              />
+              <button type="submit" onClick={handleFinish}>
+                Sign up
+              </button>
+            </form>
+
+            <p style={{ color: "#8e8e8e" }}>
+              By signing up, you agree to our <span>Terms</span>{" "}
+              <span>Privacy Policy</span> and <span>Cookies Policy.</span>
+            </p>
           </div>
         </div>
-        <div className="container">
-          <form>
-            <input type="text" name="email" ref={emailRef} />
-            <input
-              type="text"
-              name="name"
-              placeholder="Full Name"
-              ref={nameRef}
-            />
-            <input
-              type="text"
-              name="username"
-              placeholder="Username"
-              ref={userNameRef}
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              ref={passwordRef}
-            />
-            <button type="submit" onClick={handleFinish}>
-              Sign up
-            </button>
-          </form>
-
-          <p style={{ color: "#8e8e8e" }}>
-            By signing up, you agree to our <span>Terms</span>{" "}
-            <span>Privacy Policy</span> and <span>Cookies Policy.</span>
+        <div className="option">
+          <p>
+            Have an account?{" "}
+            <span
+              onClick={() => {
+                navigate("/signIn");
+              }}
+            >
+              Log in
+            </span>
           </p>
         </div>
-      </div>
-      <div className="option">
-        <p>
-          Have an account?{" "}
-          <span
-            onClick={() => {
-              navigate("/signIn");
-            }}
-          >
-            Log in
-          </span>
-        </p>
-      </div>
-      <div className="otherApps">
-        <p>Get the app.</p>
-        <div className="storeImg">
-          <img
-            src="./images/apple store.png"
-            alt=""
-            height={"40px"}
-            width={"130px"}
-          />
-          <img
-            src="./images/play store.png"
-            alt=""
-            height={"40px"}
-            width={"130px"}
-          />
+        <div className="otherApps">
+          <p>Get the app.</p>
+          <div className="storeImg">
+            <img
+              src="./images/apple store.png"
+              alt=""
+              height={"40px"}
+              width={"130px"}
+            />
+            <img
+              src="./images/play store.png"
+              alt=""
+              height={"40px"}
+              width={"130px"}
+            />
+          </div>
         </div>
       </div>
       <Footer />
